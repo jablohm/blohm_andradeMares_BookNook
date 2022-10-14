0b1b26dcac35e8321a28b922e1c67aa32cab52ed
jablohm <77591463+jablohm@users.noreply.github.com>
2022-10-14 16:44:03 -0400

    finals1 10/14

--- app/model.js
+++ app/model.js
@@ -2,50 +2,54 @@ var userInfo = {};
 
 var cart = [];
 
- 
-
 var bookList = [
   {
     // ROW 1 - BOXSETS
     bookTitle: "Twilight Boxset",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Stephenie Meyer",
     bookImg: "twilight-box-set.jpg",
     price: "$99.99",
   },
   {
     bookTitle: "Harry Potter Boxset",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "J.K. Rowling",
     bookImg: "hp-box-set.jpg",
     price: "$100",
   },
   {
     bookTitle: "Game of Thrones Boxset",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "George R.R. Martin",
     bookImg: "got-box-set.jpg",
     price: "$100",
   },
 
-   // ROW 2: BLACK HISTORY BOOKS
+  // ROW 2: BLACK HISTORY BOOKS
   {
     bookTitle: "Finding Me",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Viola Davis",
     bookImg: "finding-me.jpg",
     price: "$27.99",
   },
   {
     bookTitle: "The Autobiography of Martin Luther King, Jr. ",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Martin Luther King, Jr.",
     bookImg: "mlk-biography.jpg",
     price: "$19.99",
   },
   {
     bookTitle: "The Autobiography of Elanor Roosevelt",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Eleanor Roosevelt",
     bookImg: "eleanor-roosevelt-biography.jpg",
     price: "$17.99",
@@ -54,50 +58,76 @@ var bookList = [
   // ROW 3: HORROR BOOKS
   {
     bookTitle: "Misery",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Stephen King",
     bookImg: "misery.jpg",
     price: "$19.99",
   },
   {
     bookTitle: "Frankenstein",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Mary Shelley",
     bookImg: "frankenstein.jpg",
     price: "$15.99",
   },
   {
     bookTitle: "Phantoms",
-    bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
     bookAuthor: "Dean Koontz",
     bookImg: "phantoms.jpg",
     price: "$19.99",
   },
 
-// ROW 4: CHILDREN'S BOOKS
-{
-  bookTitle: "Winnie The Pooh",
-  bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
-  bookAuthor: "A. A. Milne",
-  bookImg: "winnie-the-pooh.jpg",
-  price: "$19.99",
-},
-{
-  bookTitle: "The Cat In The Hat",
-  bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
-  bookAuthor: "Dr. Seuss",
-  bookImg: "cat-in-the-hat.jpg",
-  price: "$15.99",
-},
-{
-  bookTitle: "Fun Facts About Space",
-  bookDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
-  bookAuthor: "Baby Professor",
-  bookImg: "fun-facts-about-space.jpg",
-  price: "$7.99",
-}, 
-
+  // ROW 4: CHILDREN'S BOOKS
+  {
+    bookTitle: "Winnie The Pooh",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookAuthor: "A. A. Milne",
+    bookImg: "winnie-the-pooh.jpg",
+    price: "$19.99",
+  },
+  {
+    bookTitle: "The Cat In The Hat",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookAuthor: "Dr. Seuss",
+    bookImg: "cat-in-the-hat.jpg",
+    price: "$15.99",
+  },
+  {
+    bookTitle: "Fun Facts About Space",
+    bookDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac.",
+    bookAuthor: "Baby Professor",
+    bookImg: "fun-facts-about-space.jpg",
+    price: "$7.99",
+  },
 ];
+
+// var homeList = [
+//   {
+//     bookImage: "to-kill-a-mockingbird.jpg",
+//     homeDesc:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nunc id cursus metus aliquam eleifend mi. Massa vitae tortor condimentum lacinia quis vel eros.",
+//     homePrice: "$15.99",
+//   },
+//   {
+//     bookImage: "becoming.jpg",
+//     homeDesc:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nunc id cursus metus aliquam eleifend mi. Massa vitae tortor condimentum lacinia quis vel eros.",
+//     homePrice: "$25.99",
+//   },
+//   {
+//     bookImage: "firestarter.jpg",
+//     homeDesc:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nunc id cursus metus aliquam eleifend mi. Massa vitae tortor condimentum lacinia quis vel eros.",
+//     homePrice: "$19.99",
+//   },
+// ];
 
 export function changePage(pageID, subPageID, callback) {
   if (pageID == "" || pageID == "home") {
@@ -107,15 +137,15 @@ export function changePage(pageID, subPageID, callback) {
       // callback();
     });
   } else if (pageID == "books") {
-    console.log("here i am")
+    // console.log("here i am")
     $.get(`pages/books/books.html`, function (data) {
-        // console.log("data " + data);
-        $("#app").html(data);
+      // console.log("data " + data);
+      $("#app").html(data);
 
       $.each(bookList, function (idx, book) {
-          if (idx <= 2) {
-            $(".bookSection").append(
-              `<div class="prodContent">
+        if (idx <= 2) {
+          $(".bookSection").append(
+            `<div class="prodContent">
               <div class="bookCover">
                <img class="bookCover" src="img/books-img/${book.bookImg}">
              </div>
@@ -126,15 +156,13 @@ export function changePage(pageID, subPageID, callback) {
                  <button id="${idx}"><a href="javascript:void(0);">ADD TO CART</a></button>
                </div> 
            </div>
-         </div>`   
-            );
-          }
-        
+         </div>`
+          );
+        }
       });
 
-      
       $.each(bookList, function (idx, book) {
-        if ((idx >= 3) && (idx < 6)) {
+        if (idx >= 3 && idx < 6) {
           $(".bookSection2").append(
             `<div class="prodContent">
             <div class="bookCover">
@@ -147,13 +175,12 @@ export function changePage(pageID, subPageID, callback) {
                <button id="${idx}"><a href="javascript:void(0);">ADD TO CART</a></button>
              </div> 
          </div>
-       </div>`   
+       </div>`
           );
         }
-      
-    });
+      });
       $.each(bookList, function (idx, book) {
-        if ((idx >= 6) && (idx < 9)) {
+        if (idx >= 6 && idx < 9) {
           $(".bookSection3").append(
             `<div class="prodContent">
             <div class="bookCover">
@@ -166,13 +193,12 @@ export function changePage(pageID, subPageID, callback) {
                <button id="${idx}"><a href="javascript:void(0);">ADD TO CART</a></button>
              </div> 
          </div>
-       </div>`   
+       </div>`
           );
         }
-      
-    });
+      });
       $.each(bookList, function (idx, book) {
-        if ((idx >= 9) && (idx < 12)) {
+        if (idx >= 9 && idx < 12) {
           $(".bookSection4").append(
             `<div class="prodContent">
             <div class="bookCover">
@@ -185,26 +211,42 @@ export function changePage(pageID, subPageID, callback) {
                <button id="${idx}"><a href="javascript:void(0);">ADD TO CART</a></button>
              </div> 
          </div>
-       </div>`   
+       </div>`
           );
         }
-      
-    });
-    if (callback) {
-      callback();
-    }
+      });
+      if (callback) {
+        callback();
+      }
     });
   } else if (subPageID != undefined) {
     $.get(`pages/${pageID}/${subPageID}.html`, function (data) {
       // console.log("data " + data);
       $("#app").html(data);
     });
-  }
-  else {
+  // } else if (pageID == "home") {
+  //   $.get(`pages/home/home.html`, function (data) {
+  //     $("app").html(data);
+
+  //     $.each(homeList, function (idx, book) {
+  //       if (idx <=2)
+  //       $(".homeBooks").append(
+  //         `<div class="homeBooks">
+  //           <img class="bookImage" src="img/home-imgs/${book.bookImage}">
+  //           <div class="bookinfo">
+  //             <p>${book.homeDesc}</p>
+  //             <h2>${book.homePrice}</h2>
+  //             <button id="${idx}">>ADD TO CART</a></button> 
+  //             </div>
+  //         </div>`
+  //       );
+  //     });
+  //   });
+  } else {
     $.get(`pages/${pageID}/${pageID}.html`, function (data) {
       // console.log("data " + data);
       $("#app").html(data);
-      $.each(cart, function(idx, cartItem){
+      $.each(cart, function (idx, cartItem) {
         console.log(bookList[cartItem]);
         let book = bookList[cartItem];
         $(".items").append(`<div class="book">
@@ -219,7 +261,6 @@ export function changePage(pageID, subPageID, callback) {
         <button id="0">${idx}</button>
     </div>
     </div>`);
-
       });
     });
   }
@@ -230,37 +271,27 @@ export function setUserInfo(userObject) {
   console.log(userInfo);
 }
 
-export function addToCart(bookIdx){
-    cart.push(bookIdx);
-    $("#cartCount").html(cart.length.toString());
+export function addToCart(bookIdx) {
+  cart.push(bookIdx);
+  $("#cartCount").html(cart.length.toString());
 
-    console.log("sub" + subPageID);
-    console.log("page" + pageID)
-  
-    if(subPageID  == undefined) {
-  
-  
-      $.get(`pages/${pageID}/${pageID}.html`, function(data){
-        // console.log(data);
-        $("#app").html(data);
-  
+  console.log("sub" + subPageID);
+  console.log("page" + pageID);
+
+  if (subPageID == undefined) {
+    $.get(`pages/${pageID}/${pageID}.html`, function (data) {
+      // console.log(data);
+      $("#app").html(data);
     }).fail((error) => {
-      if(error.status == "404"){
+      if (error.status == "404") {
         // alert("Please check your URL.");
       }
       console.log("error" + error.status);
     });
-  
-    } else {
-          $.get(`pages/${pageID}/${subPageID}.html`, function(data){
-        // console.log(data);
-        $("#app").html(data);
-    }).fail((error) => {
-  
-    })
+  } else {
+    $.get(`pages/${pageID}/${subPageID}.html`, function (data) {
+      // console.log(data);
+      $("#app").html(data);
+    }).fail((error) => {});
   }
 }
-
-
-
-
